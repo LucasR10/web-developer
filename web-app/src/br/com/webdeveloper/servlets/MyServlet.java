a65cc3597c8f0e29d62d85a21edb93ecf08c93f3
Lucas <Lucas>
2019-01-09 21:54:41 -0200

    hello servlets

--- web-app/src/br/com/webdeveloper/servlets/MyServlet.java
+++ web-app/src/br/com/webdeveloper/servlets/MyServlet.java
@@ -1,5 +1,37 @@
 package br.com.webdeveloper.servlets;
 
-public class MyServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+       System.err.println("Init servlet ");
+	}
+	 @Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	    
+		 
+		resp.setContentType("text/hml");
+	    resp.getWriter().println(" Não, não se trata so de tamanho");
+	    resp.getWriter().println(" req.getAuthType() "+ req.getAuthType() );
+	    resp.getWriter().println(" req.getCharacterEncoding() "+ req.getCharacterEncoding() );
+	    resp.getWriter().println(" req.getContextPath() "+ req.getContextPath()  );
+	    resp.getWriter().println(" req.getLocalAddr() "+ req.getLocalAddr() );
+	    resp.getWriter().println(" req.getLocalName() "+ req.getLocalName());
+	    resp.getWriter().println(" req.getRemoteUser() "+ req.getMethod() );
+	    resp.getWriter().println(" req.getRemoteUser() "+ req.getRemoteUser() );
+	    resp.getWriter().println(" resp.getContentType() "+ resp.getContentType() );
+	    resp.getWriter().println(" resp.getStatus() "+  resp.getStatus() );
+	    resp.getWriter().println(" resp.getStatus() "+  resp.getBufferSize());
+	    
+	 }
+	 
 }
